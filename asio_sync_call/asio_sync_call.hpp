02a6dc706c53fb3ec24f00d86a27b599337364aa
Dima Rusyy <dima.ru.com@gmail.com>
2019-07-19 10:32:40 +0300

    - better formatting

--- asio_sync_call/asio_sync_call.hpp
+++ asio_sync_call/asio_sync_call.hpp
@@ -8,10 +8,14 @@
 
 namespace detail
 {
-    template <typename T> struct is_chrono_duration : std::false_type {};
+    template <typename T> struct is_chrono_duration : std::false_type
+    {
+    };
 
     template <typename R, typename P>
-    struct is_chrono_duration<std::chrono::duration<R, P>> : std::true_type {};
+    struct is_chrono_duration<std::chrono::duration<R, P>> : std::true_type
+    {
+    };
 }
 
 struct asio_sync_call : std::enable_shared_from_this<asio_sync_call>
@@ -21,14 +25,18 @@ struct asio_sync_call : std::enable_shared_from_this<asio_sync_call>
     {
     }
 
-    template <typename F, typename Param,
+    template <typename F,
+        typename Param,
         typename std::enable_if<std::is_integral<Param>::value>::type* = nullptr>
-        auto wait(F handler, Param count)
+    auto wait(F handler, Param count)
     {
         std::promise<boost::system::error_code> is_done;
         auto self = this->shared_from_this();
 
-        make_async(handler, [&count]() { return count-- > 0; }, is_done);
+        make_async(handler, [&count]()
+                   {
+                       return count-- > 0;
+                   }, is_done);
 
         return is_done.get_future().get();
     }
@@ -37,7 +45,7 @@ struct asio_sync_call : std::enable_shared_from_this<asio_sync_call>
         typename Timer = boost::asio::steady_timer,
         typename Param,
         typename std::enable_if<detail::is_chrono_duration<Param>::value>::type* = nullptr>
-        auto wait(F handler, Param duration)
+    auto wait(F handler, Param duration)
     {
         std::promise<boost::system::error_code> is_done;
         const auto self = this->shared_from_this();
@@ -45,26 +53,29 @@ struct asio_sync_call : std::enable_shared_from_this<asio_sync_call>
         auto timer = std::make_shared<Timer>(_io);
         timer->expires_after(duration);
         timer->async_wait([self, timer, &is_done](const boost::system::error_code& ec)
-            {
-                if (!ec)
-                {
-                    if (timer->expires_at() == Timer::time_point::min())
-                    {
-                        self->cancel(boost::asio::error::operation_aborted);
-                    }
-                    else
-                    {
-                        self->cancel(boost::asio::error::timed_out);
-                        timer->expires_at(Timer::time_point::min());
-                    }
-                }
-                else
-                {
-                    self->cancel(ec);
-                }
-            });
+                          {
+                              if (!ec)
+                              {
+                                  if (timer->expires_at() == Timer::time_point::min())
+                                  {
+                                      self->cancel(boost::asio::error::operation_aborted);
+                                  }
+                                  else
+                                  {
+                                      self->cancel(boost::asio::error::timed_out);
+                                      timer->expires_at(Timer::time_point::min());
+                                  }
+                              }
+                              else
+                              {
+                                  self->cancel(ec);
+                              }
+                          });
 
-        make_async(handler, [timer]() { return true; }, is_done);
+        make_async(handler, [timer]()
+                   {
+                       return true;
+                   }, is_done);
 
         return is_done.get_future().get();
     }
@@ -73,10 +84,10 @@ struct asio_sync_call : std::enable_shared_from_this<asio_sync_call>
     {
         const auto self = this->shared_from_this();
         _io.dispatch([self, ec]()
-            {
-                self->_cancel = true;
-                self->_cancel_error_code = ec;
-            });
+                     {
+                         self->_cancel = true;
+                         self->_cancel_error_code = ec;
+                     });
     }
 
 private:
@@ -85,27 +96,27 @@ private:
     {
         const auto self = this->shared_from_this();
         _io.post([self, handler, predicate, &done]()
-            {
-                if (self->_cancel)
-                {
-                    done.set_value(self->_cancel_error_code);
-                    return;
-                }
+                 {
+                     if (self->_cancel)
+                     {
+                         done.set_value(self->_cancel_error_code);
+                         return;
+                     }
 
-                if (predicate())
-                {
-                    boost::system::error_code ec;
-                    handler(ec);
-                    if (ec)
-                    {
-                        self->make_async<decltype(handler)>(handler, predicate, done);
-                        return;
-                    }
-                }
+                     if (predicate())
+                     {
+                         boost::system::error_code ec;
+                         handler(ec);
+                         if (ec)
+                         {
+                             self->make_async<decltype(handler)>(handler, predicate, done);
+                             return;
+                         }
+                     }
 
-                self->cancel(make_error_code(boost::system::errc::success));
-                self->make_async<decltype(handler)>(handler, predicate, done);
-            });
+                     self->cancel(make_error_code(boost::system::errc::success));
+                     self->make_async<decltype(handler)>(handler, predicate, done);
+                 });
     }
 
     boost::asio::io_service& _io;
--- asio_sync_call/main.cpp
+++ asio_sync_call/main.cpp
@@ -12,10 +12,10 @@ protected:
     {
         _worker.reset(new boost::asio::io_service::work(_io));
         _runner = std::async(std::launch::async, [this]()
-            {
-                _io.run();
-                std::cout << "io_service::run() exited\n";
-            });
+                             {
+                                 _io.run();
+                                 std::cout << "io_service::run() exited\n";
+                             });
     }
 
     void TearDown()
@@ -34,11 +34,11 @@ TEST_F(asio_sync_call_fixture, repeat_0_times)
 
     std::size_t counter = 0;
     auto rc = t->wait([&counter](boost::system::error_code& ec)
-        {
-            ec = make_error_code(boost::asio::error::access_denied);
-            counter++;
-        },
-        0);
+                      {
+                          ec = make_error_code(boost::asio::error::access_denied);
+                          counter++;
+                      },
+                      0);
 
     ASSERT_EQ(counter, 0);
     ASSERT_TRUE(rc == boost::system::errc::success);
@@ -50,11 +50,11 @@ TEST_F(asio_sync_call_fixture, repeat_5_times)
 
     std::size_t counter = 0;
     auto rc = t->wait([&counter](boost::system::error_code& ec)
-        {
-            ec = make_error_code(boost::asio::error::access_denied);
-            counter++;
-        },
-        5);
+                      {
+                          ec = make_error_code(boost::asio::error::access_denied);
+                          counter++;
+                      },
+                      5);
 
     ASSERT_EQ(counter, 5);
     ASSERT_TRUE(rc == boost::system::errc::success);
@@ -66,12 +66,12 @@ TEST_F(asio_sync_call_fixture, repeat_5_times_exit_on_3)
 
     std::size_t counter = 0;
     auto rc = t->wait([&counter](boost::system::error_code& ec)
-        {
-            if (counter == 3) ec = make_error_code(boost::system::errc::success);
-            else ec = make_error_code(boost::asio::error::access_denied);
-            counter++;
-        },
-        5);
+                      {
+                          if (counter == 3) ec = make_error_code(boost::system::errc::success);
+                          else ec = make_error_code(boost::asio::error::access_denied);
+                          counter++;
+                      },
+                      5);
 
     ASSERT_EQ(counter, 4);
     ASSERT_TRUE(rc == boost::system::errc::success);
@@ -83,17 +83,17 @@ TEST_F(asio_sync_call_fixture, repeat_10_times_cancel)
     auto t = std::make_shared<asio_sync_call>(_io);
 
     auto async_cancel = std::async(std::launch::async, [t]()
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(300));
-            t->cancel();
-        });
+                                   {
+                                       std::this_thread::sleep_for(std::chrono::milliseconds(300));
+                                       t->cancel();
+                                   });
 
     auto rc = t->wait([](boost::system::error_code& ec)
-        {
-            ec = make_error_code(boost::asio::error::access_denied);
-            std::this_thread::sleep_for(std::chrono::milliseconds(100));
-        },
-        10);
+                      {
+                          ec = make_error_code(boost::asio::error::access_denied);
+                          std::this_thread::sleep_for(std::chrono::milliseconds(100));
+                      },
+                      10);
 
     ASSERT_EQ(rc, boost::asio::error::operation_aborted);
 }
@@ -102,24 +102,24 @@ TEST_F(asio_sync_call_fixture, wait_for_1sec_timeout)
 {
     auto t = std::make_shared<asio_sync_call>(_io);
 
-    auto ec = t->wait([&](boost::system::error_code & ec)
-        {
-            ec = make_error_code(boost::asio::error::access_denied);
-            std::this_thread::sleep_for(std::chrono::milliseconds(200));
-        },
-        std::chrono::milliseconds(1000));
+    auto ec = t->wait([&](boost::system::error_code& ec)
+                      {
+                          ec = make_error_code(boost::asio::error::access_denied);
+                          std::this_thread::sleep_for(std::chrono::milliseconds(200));
+                      },
+                      std::chrono::milliseconds(1000));
     ASSERT_EQ(ec, boost::asio::error::timed_out);
 }
 
 TEST_F(asio_sync_call_fixture, wait_for_1sec_success)
 {
     auto t = std::make_shared<asio_sync_call>(_io);
-  
+
     auto ec = t->wait([&](boost::system::error_code& ec)
-        {
-            ec = make_error_code(boost::system::errc::success);
-        },
-        std::chrono::milliseconds(1000));
+                      {
+                          ec = make_error_code(boost::system::errc::success);
+                      },
+                      std::chrono::milliseconds(1000));
     ASSERT_EQ(ec, boost::system::errc::success);
 }
 
@@ -128,16 +128,16 @@ TEST_F(asio_sync_call_fixture, wait_for_1sec_cancel)
     auto t = std::make_shared<asio_sync_call>(_io);
 
     auto async_cancel = std::async(std::launch::async, [t]()
-        {
-            std::this_thread::sleep_for(std::chrono::milliseconds(300));
-            t->cancel();
-        });
+                                   {
+                                       std::this_thread::sleep_for(std::chrono::milliseconds(300));
+                                       t->cancel();
+                                   });
 
     auto ec = t->wait([&](boost::system::error_code& ec)
-        {
-            ec = make_error_code(boost::asio::error::access_denied);
-        },
-        std::chrono::milliseconds(1000));
+                      {
+                          ec = make_error_code(boost::asio::error::access_denied);
+                      },
+                      std::chrono::milliseconds(1000));
     ASSERT_EQ(ec, boost::asio::error::operation_aborted);
 }
 
